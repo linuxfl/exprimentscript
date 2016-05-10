c59d3396e9bc809c0eabbc6155a481f41bf9f7ff
Ling.Fang <androidfl@shu.edu.com>
2016-05-10 18:32:08 +0800

    update

--- copykeyt.sh
+++ copykeyt.sh
@@ -6,6 +6,22 @@ for p in $(cat hostfile)
 do
 ip=$(echo "$p"|cut -f1 -d":")
 expect <<EOF
+spawn ssh $user@$ip "ssh-keygen -t rsa" 
+expect {
+      "yes/no" { send "yes\r";exp_continue}
+      "password:" {send "$password\r";exp_continue}
+      "Enter file in which to save the key*" {send "\r";exp_continue}
+      "Overwrite*" {send "y\r";exp_continue}
+      "Enter passphrase*" {send "\r";exp_continue}
+      "Enter same passphrase again:" {send "\r";exp_continue}
+}
+EOF
+done
+
+for p in $(cat hostfile)
+do
+ip=$(echo "$p"|cut -f1 -d":")
+expect <<EOF
 spawn scp $user@$ip:/home/$user/.ssh/id_rsa.pub /home/$user/.ssh/id_rsa.pub.$ip
 expect {
       "yes/no" { send "yes\r";exp_continue}
--- paperTest.py
+++ paperTest.py
@@ -5,15 +5,15 @@ fp = open("hostfile")
 ip_list = fp.readlines()
 
 for ip in ip_list:
-    os.system("ssh %s@%s apt-get update"%(USER,ip.strip().split(":")[0]))
+    os.system("ssh %s@%s apt-get update"%(USER,ip.strip()))
     print "ssh %s@%s apt-get update"%(USER,ip.strip())
 for ip in ip_list:
-    os.system("ssh %s@%s apt-get install -y python-pip python-numpy mpich python-dev git"%(USER,ip.strip().split(":")[0]))
-    print "ssh %s@%s apt-get install -y python-pip python-numpy mpich python-dev git"%(USER,ip.strip().split(":")[0])
+    os.system("ssh %s@%s apt-get install -y python-pip python-numpy mpich python-dev git"%(USER,ip.strip()))
+    print "ssh %s@%s apt-get install -y python-pip python-numpy mpich python-dev git"%(USER,ip.strip())
 for ip in ip_list:
-    os.system("ssh %s@%s pip install mpi4py"%(USER,ip.strip().split(":")[0]))
-    print "ssh %s@%s pip install mpi4py"%(USER,ip.strip().split(":")[0])    
+    os.system("ssh %s@%s pip install mpi4py"%(USER,ip.strip()))
+    print "ssh %s@%s pip install mpi4py"%(USER,ip.strip())
 for ip in ip_list:
-    os.system("ssh %s@%s git clone https://github.com/linuxfl/paperTest.git"%(USER,ip.strip().split(":")[0]))
-    print "ssh %s@%s git clone https://github.com/linuxfl/paperTest.git"%(USER,ip.strip().split(":")[0])
+    os.system("ssh %s@%s git clone https://github.com/linuxfl/paperTest.git"%(USER,ip.strip()))
+    print "ssh %s@%s git clone https://github.com/linuxfl/paperTest.git"%(USER,ip.strip())
 print "Success!!!"
