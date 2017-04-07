bc1711bfc9563d138cb42eeab18c0c3fc39cb9dc
fangling <871446479@qq.com>
2017-04-07 19:11:18 +0800

    a bug

--- copykeyt.sh
+++ copykeyt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-user="root"
-password="2672256ab*"
+user="fangling"
+password="0000"
 
 for p in $(cat hostfile)
 do
@@ -22,7 +22,7 @@ for p in $(cat hostfile)
 do
 ip=$(echo "$p"|cut -f1 -d":")
 expect <<EOF
-spawn scp $user@$ip:/root/.ssh/id_rsa.pub /root/.ssh/id_rsa.pub.$ip
+spawn scp $user@$ip:/home/${user}/.ssh/id_rsa.pub /home/${user}/.ssh/id_rsa.pub.$ip
 expect {
       "yes/no" { send "yes\r";exp_continue}
       "password:" {send "$password\r";exp_continue}
@@ -33,15 +33,15 @@ done
 for p in $(cat hostfile)
 do
 ip=$(echo "$p"|cut -f1 -d":")
-    cat /root/.ssh/id_rsa.pub.$ip >> /root/.ssh/authorized_keys
-    rm  /root/.ssh/id_rsa.pub.$ip
+    cat /home/${user}/.ssh/id_rsa.pub.$ip >> /home/${user}/.ssh/authorized_keys
+    rm  /home/${user}/.ssh/id_rsa.pub.$ip
 done
 
 for p in $(cat hostfile)
 do
 ip=$(echo "$p"|cut -f1 -d":")
 expect <<EOF
-spawn scp /root/.ssh/authorized_keys $user@$ip:/root/.ssh/
+spawn scp /home/${user}/.ssh/authorized_keys $user@$ip:/home/${user}/.ssh/
 expect {
       "yes/no" { send "yes\r";exp_continue}
       "password:" {send "$password\r";exp_continue}
